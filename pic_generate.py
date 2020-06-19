f399cfb5e767ea11d5adde4bbc2dcc0bc148e0c7
A-serena <52956355+A-serena@users.noreply.github.com>
2020-06-19 23:28:29 +0900

    Update pic_generate.py

--- pic_generate.py
+++ pic_generate.py
@@ -59,7 +59,7 @@ def folderselect():
     iDir = "C:\\pg" 
     global PATH
     PATH = filedialog.askdirectory(initialdir=iDir)   #指定したファイル名を取得する (キャンセル時は空文字)
-    var.set(str(PATH))
+    var.set("保存先: " + str(PATH))
     #print(PATH)
     #if folder != "":
         #tk.messagebox.showinfo(Label='folder', command=folder)
@@ -88,7 +88,9 @@ def generate(self):
         cv2.imwrite(FILE_NAME, PICTURE)
 
 def DeleteEntryValue(self):
-    #エントリーの中身を削除
+    """
+    エントリーの中身を削除する。BTN1ボタンを左クリックしたとき発動する
+    """
     HEIBOX.delete(0, tk.END)
     WIGBOX.delete(0, tk.END)
     PAGEBOX.delete(0, tk.END)
@@ -107,61 +109,78 @@ def _b5size(self): # 既に値が入ってる場合は高さ幅だけ消してB5
 ###################################################################################
 
 # GUI設定
-FRM = tk.Tk()
-FRM.geometry("300x250") # 横x縦
-FRM.title("GUIだよ")
+root = tk.Tk()
+root.geometry("300x350") # 横x縦
+root.title("GUIだよ")
 
-MENU = tk.Menu(FRM)
+MENU = tk.Menu(root)
 FILEMENU = tk.Menu(MENU, tearoff=0)
 FILEMENU.add_command(label="フォルダを指定", command=folderselect)
 FILEMENU.add_separator()
 FILEMENU.add_command(label="second", command=second)
 FILEMENU.add_command(label="third", command=third)
 FILEMENU.add_command(label="four", command=four)
-FILEMENU.add_command(label="終了(X)", command=FRM.quit)
+FILEMENU.add_command(label="終了(X)", command=root.quit)
 MENU.add_cascade(label="ファイル", menu=FILEMENU)
 
 
 # メイン部分設定
-LABEL1 = tk.Label(text="このプログラムを置いたフォルダ内に、\nタイトルと連番付きのPNG画像が生成されます。")
+frame1 = tk.Frame(root)
+frame1.pack()
+LABEL1 = tk.Label(frame1, text="タイトルと連番付きのPNG画像を生成します。")
 LABEL1.pack()
-LABEL3 = tk.Label(text="最初にどのフォルダに画像ファイルを生成するか選択してください")
+LABEL3 = tk.Label(frame1, text="最初にどのフォルダに画像ファイルを生成するか選択してください")
 LABEL3.pack()
-
 var = tk.StringVar()
-LABEL4 = tk.Label(FRM, textvariable=var)
-LABEL4.pack()
+PATH_LAB = tk.Label(frame1, textvariable=var)
+PATH_LAB.pack()
 
-lab_he = tk.Label(text="縦")
-lab_he.grid
-HEIBOX = tk.Entry()
+frame2 = tk.Frame(root)
+frame2.pack()
+frame4 = tk.Frame(root)
+frame4.pack()
+lab_he = tk.Label(frame2, text="縦(px)")
+lab_he.pack()
+HEIBOX = tk.Entry(frame4)
 HEIBOX.pack()
-WIGBOX = tk.Entry()
+
+lab_wi = tk.Label(frame2, text="横(px)")
+lab_wi.pack()
+WIGBOX = tk.Entry(frame4)
 WIGBOX.pack()
-PAGEBOX = tk.Entry()
+
+lab_pa = tk.Label(frame2, text="ページ数")
+lab_pa.pack()
+PAGEBOX = tk.Entry(frame4)
 PAGEBOX.pack()
-TITBOX = tk.Entry()
+
+lab_ti = tk.Label(frame2, text="タイトル(半角英数字)")
+lab_ti.pack()
+TITBOX = tk.Entry(frame4)
 TITBOX.pack()
 
+frame3 = tk.Frame(root)
+frame3.pack()
+LABEL2 = tk.Label(frame3, text="左クリックで生成開始\n右クリックで入力内容の削除")
+LABEL2.pack()
 
-BTN1 = tk.Button(FRM, text="生成！")
+BTN1 = tk.Button(frame3, text="生成！")
 BTN1.bind("<Button-1>", generate) # 左クリックで入力
 BTN1.bind("<Button-3>", DeleteEntryValue) # 右クリックでEntry内を削除
-BTN1.pack()
+BTN1.pack(side="left")
 
-BTN2 = tk.Button(FRM, text="B5サイズ")
+BTN3 = tk.Button(frame3, text="保存")
+BTN3.bind("<Button-1>", a)
+BTN3.pack(side="left")
+
+BTN2 = tk.Button(frame3, text="B5サイズ") # ここに表示するもの増えそうだから、何らかのリストにしたい
 BTN2.bind("<Button-1>", _b5size) # 左クリックで入力
 BTN2.pack()
 
-BTN3 = tk.Button(FRM, text="保存")
-BTN3.bind("<Button-1>", a)
-BTN3.pack()
 
-LABEL2 = tk.Label(text="左クリックで生成開始\n右クリックで入力内容の削除")
-LABEL2.pack()
 
 ###################################################################################
 
 
-FRM.config(menu=MENU)
-FRM.mainloop()
+root.config(menu=MENU)
+root.mainloop()
