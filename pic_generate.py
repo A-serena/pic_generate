6348fce3f1a0f2b89f1ec5111e2f8160ee7061e6
A-serena <52956355+A-serena@users.noreply.github.com>
2020-07-01 01:39:19 +0900

    GUIを大方整えた

--- pic_generate.py
+++ pic_generate.py
@@ -34,7 +34,7 @@ def a():
 
     s = [たかさ, はば, ぺーじ, たいとる]
     path = os.getcwd()
-    with open(path, mode="a", encoding="utf-8") as f:
+    with open(path, mode="a", encoding="utf-8") as f: # この設定で開いたこれを f とするという意味
         f.write("\n".join(s))
 
 
@@ -111,6 +111,7 @@ def _b5size(self): # 既に値が入ってる場合は高さ幅だけ消してB5
 # GUI設定
 root = tk.Tk()
 root.geometry("300x350") # 横x縦
+root.propagate(False)
 root.title("GUIだよ")
 
 MENU = tk.Menu(root)
@@ -126,7 +127,6 @@ MENU.add_cascade(label="ファイル", menu=FILEMENU)
 
 # メイン部分設定
 frame1 = tk.Frame(root)
-frame1.pack()
 LABEL1 = tk.Label(frame1, text="タイトルと連番付きのPNG画像を生成します。")
 LABEL1.pack()
 LABEL3 = tk.Label(frame1, text="最初にどのフォルダに画像ファイルを生成するか選択してください")
@@ -135,51 +135,58 @@ var = tk.StringVar()
 PATH_LAB = tk.Label(frame1, textvariable=var)
 PATH_LAB.pack()
 
-frame2 = tk.Frame(root)
-frame2.pack()
-frame4 = tk.Frame(root)
-frame4.pack()
+
+Entrys = tk.Frame(root)
+
+frame2 = tk.Frame(Entrys)
+frame3 = tk.Frame(Entrys)
 lab_he = tk.Label(frame2, text="縦(px)")
 lab_he.pack()
-HEIBOX = tk.Entry(frame4)
+HEIBOX = tk.Entry(frame3)
 HEIBOX.pack()
 
 lab_wi = tk.Label(frame2, text="横(px)")
 lab_wi.pack()
-WIGBOX = tk.Entry(frame4)
+WIGBOX = tk.Entry(frame3)
 WIGBOX.pack()
 
 lab_pa = tk.Label(frame2, text="ページ数")
 lab_pa.pack()
-PAGEBOX = tk.Entry(frame4)
+PAGEBOX = tk.Entry(frame3)
 PAGEBOX.pack()
 
 lab_ti = tk.Label(frame2, text="タイトル(半角英数字)")
 lab_ti.pack()
-TITBOX = tk.Entry(frame4)
+TITBOX = tk.Entry(frame3)
 TITBOX.pack()
 
-frame3 = tk.Frame(root)
-frame3.pack()
-LABEL2 = tk.Label(frame3, text="左クリックで生成開始\n右クリックで入力内容の削除")
+frame4 = tk.Frame(root)
+LABEL2 = tk.Label(frame4, text="左クリックで生成開始\n右クリックで入力内容の削除")
 LABEL2.pack()
 
-BTN1 = tk.Button(frame3, text="生成！")
+BTN1 = tk.Button(frame4, text="生成！")
 BTN1.bind("<Button-1>", generate) # 左クリックで入力
 BTN1.bind("<Button-3>", DeleteEntryValue) # 右クリックでEntry内を削除
 BTN1.pack(side="left")
 
-BTN3 = tk.Button(frame3, text="保存")
+BTN3 = tk.Button(frame4, text="保存")
 BTN3.bind("<Button-1>", a)
 BTN3.pack(side="left")
 
-BTN2 = tk.Button(frame3, text="B5サイズ") # ここに表示するもの増えそうだから、何らかのリストにしたい
+BTN2 = tk.Button(frame4, text="B5サイズ") # ここに表示するもの増えそうだから、何らかのリストにしたい
 BTN2.bind("<Button-1>", _b5size) # 左クリックで入力
 BTN2.pack()
 
 
-
 ###################################################################################
+frame1.pack(side="top") # 一番上の文字とパス 上詰めで
+
+frame2.pack(fill="none", side="left") # 各入力欄の説明書き
+frame3.pack(fill="none", side="left")
+Entrys.pack(side="top")
+
+frame4.pack(side="top")
+
 
 
 root.config(menu=MENU)
